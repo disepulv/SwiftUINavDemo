d5ae939c9569e93e08cb376f245b3babd0e111ac
ladiego.sepulveda <ladiego.sepulveda@ladiegosepulvedas-MacBook-Pro.local>
2021-12-01 08:17:28 -0300

    coach: seasion 1

--- SwitfUINavDemo/Views/ContentView.swift
+++ SwitfUINavDemo/Views/ContentView.swift
@@ -7,9 +7,32 @@
 
 import SwiftUI
 
+class NavObject: ObservableObject {
+    @Published var isDisplayingChildren = false
+}
+
+struct ThirdView: View {
+    @EnvironmentObject var navObj: NavObject
+    
+    var body: some View {
+        VStack {
+            Text("Some other view!")
+            NavigationLink("Next", destination: ThirdView().environmentObject(navObj))
+        }.toolbar(content: {
+            ToolbarItemGroup {
+                Button(action: { navObj.isDisplayingChildren.toggle() }, label: {
+                    Text("Pop Back")
+                })
+            }
+        })
+    }
+}
+
+
 struct ContentView: View {
     @State var isPresentingView = false
-    
+    @StateObject private var navObject = NavObject()
+
     var body: some View {
         NavigationView {
             VStack(spacing: 30) {
@@ -21,6 +44,18 @@ struct ContentView: View {
                         print("pending...")
                     }
                 }
+                
+                
+                NavigationLink(
+                    destination:
+                        ThirdView().environmentObject(navObject)
+                    ,
+                    isActive: $navObject.isDisplayingChildren,
+                    label: {
+                        Text("Navigate")
+                    }
+                )
+                
                 NavigationLink(destination: SecondView(isPresentingView: $isPresentingView, param1: "Pushed")) {
                     Text("Push to next view")
                 }
